07cf7e35ff0d7d2290e95bcec30ba307e6c47a6a
azarll <sebastien.dureuil89@gmail.com>
2023-03-20 15:43:10 +0100

    ✏️ F01: LOGIN > Form > Fix typo

--- src/Components/pages/login/LoginForm.js
+++ src/Components/pages/login/LoginForm.js
@@ -18,7 +18,7 @@ export default function LoginForm() {
 //   render
   return (
     <form action="submit" onSubmit={handleSubmit}>
-      <h1>Bienvenue chez nous</h1>
+      <h1>Bienvenue chez nous !</h1>
       <br />
       <h2>Connectez-vous</h2>
       <input
