28f040febd98801bf9d038700a22a3ba095587e4
azarll <sebastien.dureuil89@gmail.com>
2023-03-24 10:04:21 +0100

    💄 F03 : LOGIN > LoginForm > Add styling

--- src/Components/pages/login/LoginForm.js
+++ src/Components/pages/login/LoginForm.js
@@ -21,7 +21,7 @@ export default function LoginForm() {
   return (
     <LoginFormStyled action="submit" onSubmit={handleSubmit}>
       <h1>Bienvenue chez nous !</h1>
-      <br />
+      <hr />
       <h2>Connectez-vous</h2>
       <input
         value={inputValue}
@@ -37,4 +37,30 @@ export default function LoginForm() {
 
 const LoginFormStyled = styled.form`
   background: green;
+  text-align: center;
+  max-width: 500px;
+  min-height: 400px;
+  margin: 0 auto;
+  padding: 2.5rem 2 rem;
+  border-radius: 5px;
+  font-family: "Amatic SC", cursive;
+
+  hr {
+    border: 1.5px solid #f56a2c;
+    margin-bottom: 40px;
+
+  }
+
+  h1 {
+    color: white;
+    font-size: 48px;
+  }
+
+  h2 {
+    color: #8e8b8b;
+    margin: 20px 10px 10px;
+    color: white;
+    font-size: 36px;
+
+  }
 `;
