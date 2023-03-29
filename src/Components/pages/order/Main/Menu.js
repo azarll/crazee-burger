5c54ab762df633a86bd828a0b5c7cd64505c785d
azarll <sebastien.dureuil89@gmail.com>
2023-03-29 22:17:12 +0200

    🍱 F05: Add Assets

--- src/Components/pages/order/Main/Menu.js
+++ src/Components/pages/order/Main/Menu.js
@@ -1,6 +1,15 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 export default function Menu() {
+
+// state
+const [menu, setproducts] = useState()
+
+// Action
+
+
+// Render
   return <MenuStyled className="menu">Menu</MenuStyled>;
 }
 
