3aff0ef05d39fce9eb9d4bf4e25d150dc6acb540
azarll <sebastien.dureuil89@gmail.com>
2023-03-29 22:04:33 +0200

    💡 F05: ORDER > Main > Prepare code for <basket/>

--- src/Components/pages/order/Main/Main.js
+++ src/Components/pages/order/Main/Main.js
@@ -5,7 +5,7 @@ import Menu from "./Menu";
 export default function Main() {
   return (
     <MainStyled>
-      <div className="basket">basket</div>
+      {/* <div className="basket">basket</div> */}
       <Menu />
     </MainStyled>
   );
@@ -20,11 +20,11 @@ const MainStyled = styled.div`
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 
   display: grid;
-  grid-template-columns: 25% 1fr;
+  grid-template-columns: 1fr;
 
-  basket {
+  /* basket {
     background: pink;
-  }
+  } */
 
 
 `;
