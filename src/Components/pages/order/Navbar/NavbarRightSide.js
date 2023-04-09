8cb446f56669307effb1a67893b0bb06bf6df6d2
azarll <sebastien.dureuil89@gmail.com>
2023-04-10 00:39:21 +0200

    ♻️ F06: NAVBAR > RightSide > Extract ToastAdmin

--- src/Components/pages/order/Navbar/NavbarRightSide.js
+++ src/Components/pages/order/Navbar/NavbarRightSide.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 import Profile from "./Profile";
 import ToggleButton from "../../../reusable-ui/ToggleButton";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import { theme } from "../../../../theme";
 import { useState } from "react";
+import ToastAdmin from "./ToastAdmin";
+import { toast } from "react-toastify";
 
 export default function NavbarRightSide({ username }) {
   const [isModeAdmin, setIsModeAdmin] = useState(false);
@@ -34,7 +34,8 @@ export default function NavbarRightSide({ username }) {
         onToggle={displayToastNotification}
       />
       <Profile username={username} />
-      <ToastContainer className="toaster" bodyClassName="body-toast" />
+      <ToastAdmin />
+
     </NavbarRightSideStyled>
   );
 }
@@ -43,22 +44,4 @@ const NavbarRightSideStyled = styled.div`
   display: flex;
   align-items: center;
   padding-right: 50px;
-
-  .toaster {
-    max-width: 300px;
-  }
-
-  .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
-    background: ${theme.colors.background_dark};
-  }
-
-  .body-toast {
-    .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
-      margin-right: 20px;
-      margin-left: 5px;
-    }
-    div {
-      line-height: 1.3em;
-    }
-  }
 `;
